061f33d50991a8b5638dcc7be92748b318fb614a
lylee76 <pakhrinamylee@gmail.com>
2024-12-29 07:27:30 +0545

    admin delete/add/update

---
+++ features/Admin/index.php
@@ -0,0 +1,39 @@
+<?php include('partials/header.php'); ?>
+
+<!-- Main content section starts here-->
+<div class="Main-content">
+    <div class="wrapper" >
+    <h2>DASHBOARD</h2>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br />
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br />
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br />
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br />
+            Categories
+        </div>
+
+        <div class="clearfix"></div>
+    
+
+    </div>
+</div>
+<!-- Main Content section ends here-->
+
+<?php include('partials/footer.php')?>
---
+++ features/Admin/partials/footer.php
@@ -0,0 +1,11 @@
+<!-- Footer section starts here-->
+<div class="footer">
+    <div class="wrapper" >
+    <p class="text-center">©2024 Bloomify of Gurung and lylee. All rights reserved.
+</p>
+    </div>
+</div>
+<!-- Footer section ends here-->
+
+</body>
+</html>
