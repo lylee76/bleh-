6945317d503cc5613b178423eb080bc74f91ec0c
lylee76 <pakhrinamylee@gmail.com>
2025-01-13 11:24:44 +0545

    admin added

---
+++ features/Admin/Admin/add-admin.php
@@ -0,0 +1,87 @@
+<?php include('partials/header.php'); ?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h2>Add Admin</h2>
+        <br>
+    <?php
+    if(isset($_SESSION['add']))
+    {
+        echo $_SESSION['add'];//displaying session message
+        unset($_SESSION['add']);//removing session messages
+    }
+    ?>
+    <br><br>
+
+        <form action="" method="POST">
+
+        <table class="tbl-30">
+            <tr>
+                <td>Full Name:</td>
+                <td>
+                    <input type="text" name="full_name" placeholder="Enter Your Name">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Username:</td>
+                <td>
+                    <input type="text" name="username" placeholder="Enter Your Username">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Password:</td>
+                <td>
+                    <input type="password" name="password" placeholder="Enter Your Password">
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="2">
+                    <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
+
+                </td>
+            </tr>
+        </table>
+        </form>
+
+    </div>
+</div>
+
+<?php include('partials/footer.php');?>
+
+<?php 
+
+//check whether the submit button is clicked or not
+
+if(isset($_POST['submit']))
+{
+    //1.get the form data
+    $full_name = $_POST['full_name'];
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);//password encryption
+
+    //2.sql query to save the data into the database
+    $sql = "INSERT INTO tbl_admin (full_name, username, password) VALUES ('$full_name', '$username', '$password')";
+
+    //3. execute the query amd save the data into the database
+    $res = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+    
+    //4 check whether the query is executed or not 
+    if($res==true)
+    {
+        //create session variable to display the message
+        $_SESSION['add'] = "Admin added successfully";
+        //redirect to the manage admins page
+        header('location:'.SITEURL.'admin/manage.admin.php');
+    }
+    else{
+         //create session variable to display the message
+        $_SESSION['add'] = "Failed to Add admin";
+         //redirect to the add admin page
+        header('location:'.SITEURL.'admin/add-admin.php');
+    }
+}
+
+?>
---
+++ features/Admin/Admin/manage.admin.php
@@ -0,0 +1,94 @@
+<?php include('partials/header.php'); ?>
+
+
+<!-- Main content section starts here-->
+<div class="Main-content">
+    <div class="wrapper" >
+    <h2>Manage Admin</h2>
+    <br>
+
+    <?php
+    if(isset($_SESSION['add']))
+    {
+        echo $_SESSION['add'];//displaying session message
+        unset($_SESSION['add']);//removing session messages
+    }
+
+    if(isset($_SESSION['delete']))
+    {
+        echo $_SESSION['delete'];//displaying session message
+        unset($_SESSION['delete']);//removing session messages
+    }
+
+    ?>
+
+
+    <br><br>
+    <!-- button to add admin-->
+    <a href="add-admin.php" class="btn-primary">Add Admin</a>
+    <br><br><br>
+    
+    <table class="tbl-full">
+        <tr>
+            <th>S.N.</th>
+            <th>Full Name</th>
+            <th>Username</th>
+            <th>Actions</th>
+        </tr>
+
+        <?php
+        //query to get all the admin
+        $sql = "SELECT * FROM tbl_admin";
+        //execute the query
+        $res= mysqli_query($conn, $sql);
+
+        //check where the query is executed or not
+        if($res==true)
+        {
+            //get the number of rows
+            $count = mysqli_num_rows($res);//function to get the number of rows
+            
+            $sn= 1;
+            //check the number of rows
+            if($count>0);
+            {
+                while($rows = mysqli_fetch_assoc($res))
+                {
+                    //get individual data
+                    $id = $rows['id'];
+                    $full_name = $rows['full_name'];
+                    $username = $rows['username'];
+
+                    //display the values in the table
+                    ?>
+
+                    <tr>
+                        <td><?php echo $sn++;?></td>
+                        <td><?php echo $full_name;?></td>
+                        <td><?php echo $username;?></td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+
+                    <?php
+
+                }
+            }
+    
+            
+        
+        
+        }
+        ?>
+
+        
+    
+    </table>
+    </div>
+</div>
+<!-- Main Content section ends here-->
+
+
+<?php include('partials/footer.php');?>
