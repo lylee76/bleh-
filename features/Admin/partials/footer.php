3042e80bf8d3a3891e5bc12f33dcd836d48352cd
lylee76 <pakhrinamylee@gmail.com>
2025-01-13 13:31:07 +0545

    Revert "Merge branch 'main' of https://github.com/lylee76/bleh- into bleh"
    This reverts commit 80c4e26db5d206ff3f54420576860040dfb40fdf, reversing
    changes made to 05681653945bca70e6223238378945376519c15c.

--- features/Admin/partials/footer.php
+++ features/Admin/partials/footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- Footer section starts here-->
 <div class="footer">
     <div class="wrapper" >
@@ -10,16 +9,3 @@
 
 </body>
 </html>
-=======
-<!-- Footer section starts here-->
-<div class="footer">
-    <div class="wrapper" >
-    <p class="text-center">©2024 Bloomify of Gurung and lylee. All rights reserved.
-</p>
-    </div>
-</div>
-<!-- Footer section ends here-->
-
-</body>
-</html>
->>>>>>> bbfb406ad95cf43132de031fbc9a96f54e27315c
