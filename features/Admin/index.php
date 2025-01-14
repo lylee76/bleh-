c66c086c7a694d20706f606b23815164173633b9
lylee76 <pakhrinamylee@gmail.com>
2025-01-14 10:42:36 +0545

    dashboard updated

--- features/Admin/index.php
+++ features/Admin/index.php
@@ -1,4 +1,3 @@
-
 <?php include('partials/header.php'); ?>
 
 <!-- Main content section starts here-->
@@ -16,27 +15,69 @@
     <br><br>
 
         <div class="col-4 text-center">
-            <h1>5</h1>
+
+            <?php
+                //sql query
+                $sql = "SELECT * FROM tbl_category";
+                //execute the query
+                $result = mysqli_query($conn, $sql);
+                //get the number of rows
+                $count = mysqli_num_rows($result);
+            ?>
+
+            <h1><?php echo $count; ?></h1>
             <br />
             Categories
         </div>
 
         <div class="col-4 text-center">
-            <h1>5</h1>
+
+                <?php
+                    //sql query
+                    $sql2 = "SELECT * FROM tbl_product";
+                    //execute the query
+                    $result2 = mysqli_query($conn, $sql2);
+                    //get the number of rows
+                    $count2 = mysqli_num_rows($result2);
+                ?>
+            <h1><?php echo $count2;?></h1>
             <br />
-            Categories
+            Products
         </div>
 
         <div class="col-4 text-center">
-            <h1>5</h1>
+
+                <?php
+                    //sql query
+                    $sql3 = "SELECT * FROM tbl_order";
+                    //execute the query
+                    $result3 = mysqli_query($conn, $sql3);
+                    //get the number of rows
+                    $count3 = mysqli_num_rows($result3);
+                ?>
+            <h1><?php echo $count2;?></h1>
             <br />
-            Categories
+            Total Orders
         </div>
 
         <div class="col-4 text-center">
-            <h1>5</h1>
+
+            <?php
+                //sql query to get total revenue generated
+                //aggregate function in sql
+                $sql4 = "SELECT SUM(total) as TOTAL FROM tbl_order WHERE status ='delivered'";
+                //execute the query
+                $result4 = mysqli_query($conn, $sql4);
+
+                //fetch the data
+                $row4 = mysqli_fetch_assoc($result4);
+                
+                //get the total revenue from the fetched data
+                $total_revenue = $row4['TOTAL'];
+            ?>
+            <h1>$<?php echo $total_revenue;?></h1>
             <br />
-            Categories
+            Revenue Generated
         </div>
 
         <div class="clearfix"></div>
